3c0743bbe0e0fefce31a86047d9e30f0962e2e5e
Icaro <iba1987@gmail.com>
2023-10-03 23:42:27 -0300

    feat(list-logs): add endpoint to get logs list.

--- src/main/java/com/labmedical/backend/services/LogService.java
+++ src/main/java/com/labmedical/backend/services/LogService.java
@@ -1,5 +1,7 @@
 package com.labmedical.backend.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,9 @@ public class LogService {
 
     public void saveLog(Log log) {
         this.logRepository.save(log);
+    }
+
+    public List<Log> getLogs() {
+        return this.logRepository.findAllByOrderByCreatedAtDesc();
     }
 }
