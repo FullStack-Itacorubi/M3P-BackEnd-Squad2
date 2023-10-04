e28f34b1b46b05c801697ca91b0507de3bdc43d6
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-10-04 11:48:19 -0500

    fix(Person): Acrecentar validacoes com mensagems de erro, regex, e tipos de atributo

--- src/main/java/com/labmedical/backend/entities/Person.java
+++ src/main/java/com/labmedical/backend/entities/Person.java
@@ -1,9 +1,7 @@
 package com.labmedical.backend.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,10 +16,32 @@ public abstract class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name cannot exceed 255 characters")
     private String name;
-    private String gender;
+
+    @NotNull(message = "Gender is required")
+    private Gender gender;
+
+    @NotBlank(message = "CPF is required")
+    @Column(unique = true)
+    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "Invalid CPF format")
     private String cpf;
+
+    @Pattern(regexp = "\\(\\d{2}\\)\\s?\\d{4,5}-\\d{4}", message = "Invalid phone number format (e.g., (XX) XXXX-XXXX or (XX) XXXXX-XXXX)")
     private String phone;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email address")
     private String email;
-    private String status;
+
+    @NotNull(message = "Status is required")
+    private Boolean status;
+
+    public enum Gender {
+        MALE,
+        FEMALE,
+        OTHER
+    }
 }
