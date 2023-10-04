3a797891d15c112e3a4711d165daec5cef09c6ff
Icaro <iba1987@gmail.com>
2023-10-03 23:01:45 -0300

    feat(save-log): create service func to save log

--- src/main/java/com/labmedical/backend/services/LogService.java
+++ src/main/java/com/labmedical/backend/services/LogService.java
@@ -1,8 +1,18 @@
 package com.labmedical.backend.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.labmedical.backend.entities.Log;
+import com.labmedical.backend.repositories.LogRepository;
 
 @Service
 public class LogService {
 
+    @Autowired
+    private LogRepository logRepository;
+
+    public void saveLog(Log log) {
+        this.logRepository.save(log);
+    }
 }
