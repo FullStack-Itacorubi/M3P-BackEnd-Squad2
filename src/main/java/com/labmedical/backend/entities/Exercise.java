58243f89d8480ff47e0cfc20bd6acae1c74d5596
Pedro Alencar <pedromariutti@gmail.com>
2023-10-05 00:28:37 -0300

    initial atributes and validations to Exercise entity

---
+++ src/main/java/com/labmedical/backend/entities/Exercise.java
@@ -0,0 +1,62 @@
+package com.labmedical.backend.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Entity
+@Table(name = "tb_exercise")
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @NotBlank(message = "Exercise name is required")
+    @Size(min = 5, max = 100, message = "Exersise name must be between 05 and 100 characters")
+    @Column(name = "exercise_name")
+    private String exerciseName;
+
+    @NotNull(message = "Date is required")
+    @Column(name = "date")
+    private LocalDate date;
+
+    @NotNull(message = "Time is required")
+    @Column(name = "time")
+    private LocalTime time;
+
+    @NotNull(message = "Type is required")
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @NotNull(message= "Exercise weekly amount value may not be empty")
+    @Column(name = "weekly_amount",nullable = false)
+    @Range(min = 1, max = 7, message = "Exercise weekly amount value must be between 1 and 7")
+    private Integer weeklyAmount;
+
+    @NotBlank(message = "Exercise name is required")
+    @Size(min = 10, max = 1000, message = "Exercise description must be between 10 and 1000 characters")
+    @Column(name = "description")
+    private String description;
+
+    @NotNull(message = "System Status is required")
+    @Column(name = "system_status")
+    private Boolean systemStatus = true;
+
+
+
+
+    public enum Type {
+        AEROBIC_RESISTANCE,
+        MUSCULAR_RESISTANCE,
+        FLEXIBILITY,
+        STRENGTH,
+        AGILITY,
+        OTHER
+    }
+}
