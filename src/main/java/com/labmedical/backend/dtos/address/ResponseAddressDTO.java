f9fc569a79e9ee06d39602a2f458f4cfa3f06b56
Pedro Alencar <pedromariutti@gmail.com>
2023-10-07 16:21:41 -0300

    feat(save-log): create service func to save log

---
+++ src/main/java/com/labmedical/backend/dtos/address/ResponseAddressDTO.java
@@ -0,0 +1,38 @@
+package com.labmedical.backend.dtos.address;
+
+import com.labmedical.backend.entities.Address;
+
+public record ResponseAddressDTO(
+        Long id,
+
+        String cep,
+
+        String city,
+
+        String state,
+
+        String street,
+
+        String number,
+
+        String complement,
+
+        String district,
+
+        String reference
+) {
+
+    public ResponseAddressDTO(Address address) {
+        this(
+                address.getId(),
+                address.getCep(),
+                address.getCity(),
+                address.getState(),
+                address.getStreet(),
+                address.getNumber(),
+                address.getComplement(),
+                address.getDistrict(),
+                address.getReference()
+        );
+    }
+}
