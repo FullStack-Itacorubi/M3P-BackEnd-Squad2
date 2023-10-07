a894925ac8cdfc6e1587a9c21f3a516b92da1b44
Pedro Alencar <pedromariutti@gmail.com>
2023-10-07 16:57:28 -0300

    feat(findById-address): create a service func to find a address by id and a dto response to it

--- src/main/java/com/labmedical/backend/services/AddressService.java
+++ src/main/java/com/labmedical/backend/services/AddressService.java
@@ -1,9 +1,12 @@
 package com.labmedical.backend.services;
 
+import com.labmedical.backend.dtos.address.ResponseAddressDTO;
 import com.labmedical.backend.entities.Address;
 import com.labmedical.backend.repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AddressService {
@@ -13,5 +16,11 @@ public class AddressService {
 
     public void saveAddress(Address address){
        this.addressRepository.save(address);
+    }
+
+    public ResponseAddressDTO findAddresById(Long addressId) {
+        return new ResponseAddressDTO(this.addressRepository.findById(addressId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Address not found"))
+        );
     }
 }
