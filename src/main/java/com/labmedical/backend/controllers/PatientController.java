241acc13cf6b540b5bdd5ac582564ffb8945d58e
Pedro Alencar <pedromariutti@gmail.com>
2023-10-09 00:03:39 -0300

    fix(save-patient): add Exceptions to patient controller

--- src/main/java/com/labmedical/backend/controllers/PatientController.java
+++ src/main/java/com/labmedical/backend/controllers/PatientController.java
@@ -3,14 +3,17 @@ package com.labmedical.backend.controllers;
 import com.labmedical.backend.dtos.patients.PostRequestPatientDTO;
 import com.labmedical.backend.dtos.patients.PostResponsePatientDTO;
 import com.labmedical.backend.services.PatientService;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("patients")
@@ -21,16 +24,16 @@ public class PatientController {
 
     @PostMapping
     public ResponseEntity<PostResponsePatientDTO> savePatient(@RequestBody @Valid PostRequestPatientDTO patient) {
-//        try {
+        try {
             return new ResponseEntity<>(patientService.savePatient(patient), HttpStatus.CREATED);
-//        } catch (DataIntegrityViolationException ex) {
-//            throw new ResponseStatusException(
-//                    HttpStatus.CONFLICT, "Duplicated data: CPF or e-mail already exists", ex);
-//        } catch (ConstraintViolationException ex) {
-//            throw new ResponseStatusException(
-//                    HttpStatus.BAD_REQUEST, "Invalid data", ex);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+        } catch (DataIntegrityViolationException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT, "Duplicated data: CPF or e-mail already exists", ex);
+        } catch (ConstraintViolationException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Invalid data", ex);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
