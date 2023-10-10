a0dbe48f56c816d8a77658fa076b63149ff9e44d
Pedro Alencar <pedromariutti@gmail.com>
2023-10-09 23:17:02 -0300

    feature(update-patient): add endpoins and func to controller and service. Add a PutRequest DTO and func mapper

---
+++ src/main/java/com/labmedical/backend/dtos/patients/PutRequestPatientDTO.java
@@ -0,0 +1,66 @@
+package com.labmedical.backend.dtos.patients;
+
+import com.labmedical.backend.entities.Address;
+import com.labmedical.backend.entities.Patient;
+import com.labmedical.backend.entities.Person;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import java.time.LocalDate;
+
+public record PutRequestPatientDTO(
+
+        Long id,
+
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @NotNull(message = "Gender is required")
+        Person.Gender gender,
+
+        @NotBlank(message = "CPF is required")
+        String cpf,
+
+        @Pattern(regexp = "\\(\\d{2}\\)\\s?\\d{4,5}-\\d{4}", message = "Invalid phone number format (e.g., (XX) XXXX-XXXX or (XX) XXXXX-XXXX)")
+        @NotBlank(message = "Phone number is required")
+        String phone,
+
+        @Email(message = "Invalid email address")
+        @NotBlank(message = "Email is required")
+        String email,
+
+        @NotNull(message = "Patient system status is required")
+        Boolean status,
+
+        @NotNull(message = "Date of Birth is required")
+        LocalDate dateOfBirth,
+
+        @NotBlank(message = "RG is required")
+        String rgWithIssuingAuthority,
+
+        @NotNull(message = "Marital Status is required")
+        Patient.MaritalStatus maritalStatus,
+
+        @NotBlank(message = "Emergency contact number is required")
+        String emergencyContact,
+
+        String allergies,
+
+        @Column(name = "specific_care")
+        String specificCare,
+
+        String insurance,
+
+        @Column(name = "insurance_number")
+        String insuranceNumber,
+
+        @Column(name = "insurance_validity")
+        LocalDate insuranceValidity,
+
+        @NotNull(message = "Address is required")
+        Address address
+) {
+}
--- src/main/java/com/labmedical/backend/services/PatientService.java
+++ src/main/java/com/labmedical/backend/services/PatientService.java
@@ -3,14 +3,13 @@ package com.labmedical.backend.services;
 import com.labmedical.backend.dtos.patients.GetResponsePatientDTO;
 import com.labmedical.backend.dtos.patients.PostRequestPatientDTO;
 import com.labmedical.backend.dtos.patients.PostResponsePatientDTO;
+import com.labmedical.backend.dtos.patients.PutRequestPatientDTO;
 import com.labmedical.backend.entities.Patient;
 import com.labmedical.backend.mappers.AddressMapper;
 import com.labmedical.backend.mappers.PatientMapper;
 import com.labmedical.backend.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -48,5 +47,16 @@ public class PatientService {
 
     public GetResponsePatientDTO findPatientById(Long id) {
         return  patientMapper.mapToGetResponsePatientDTO(patientRepository.findById(id).get());
+    }
+
+
+    public PostResponsePatientDTO replacePatientData(Long id, PutRequestPatientDTO patient) {
+
+        Patient patientToUpdate = patientMapper.map(findPatientById(id));
+        Patient newDataPatient = patientMapper.map(patient);
+        newDataPatient.setId(id);
+
+
+        return patientMapper.mapToPostResponsePatientDTO(patientRepository.save(newDataPatient));
     }
 }
