db51591beb67db5949e5aeb64dda9f7cb54e1505
Pedro Alencar <pedromariutti@gmail.com>
2023-10-09 22:12:26 -0300

    fix(list-patient): fix DTO and params

--- src/main/java/com/labmedical/backend/dtos/patients/GetResponsePatientDTO.java
+++ src/main/java/com/labmedical/backend/dtos/patients/GetResponsePatientDTO.java
@@ -1,4 +1,43 @@
 package com.labmedical.backend.dtos.patients;
 
-public record GetResponsePatientDTO() {
+import com.labmedical.backend.entities.Address;
+import com.labmedical.backend.entities.Patient;
+import com.labmedical.backend.entities.Person;
+
+import java.time.LocalDate;
+
+public record GetResponsePatientDTO(
+        Long id,
+
+        String name,
+
+        Person.Gender gender,
+
+        String cpf,
+
+        String phone,
+
+        String email,
+
+        Boolean status,
+
+        LocalDate dateOfBirth,
+
+        String rgWithIssuingAuthority,
+
+        Patient.MaritalStatus maritalStatus,
+
+        String emergencyContact,
+
+        String allergies,
+
+        String specificCare,
+
+
+        String insuranceNumber,
+
+        LocalDate insuranceValidity,
+
+        Address address
+) {
 }
