14ad2af80198331082a78c6d3520aa1761a30b2c
Pedro Alencar <pedromariutti@gmail.com>
2023-10-14 18:58:54 -0300

    fix: delete not used imports

--- src/main/java/com/labmedical/backend/services/ExamServiceImpl.java
+++ src/main/java/com/labmedical/backend/services/ExamServiceImpl.java
@@ -7,9 +7,7 @@ import com.labmedical.backend.entities.Exam;
 import com.labmedical.backend.mappers.ExamMapper;
 import com.labmedical.backend.repositories.ExamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
