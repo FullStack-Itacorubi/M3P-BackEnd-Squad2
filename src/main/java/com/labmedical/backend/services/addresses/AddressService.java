da26d2e360c2545aa02f802bac396253ce504c8f
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-10-22 23:02:47 -0500

    Revert "fix: config exception messages related with address"

--- src/main/java/com/labmedical/backend/services/addresses/AddressService.java
+++ src/main/java/com/labmedical/backend/services/addresses/AddressService.java
@@ -2,12 +2,30 @@ package com.labmedical.backend.services.addresses;
 
 import com.labmedical.backend.dtos.address.RequestAddressDTO;
 import com.labmedical.backend.dtos.address.ResponseAddressDTO;
+import com.labmedical.backend.entities.Address;
+import com.labmedical.backend.mappers.AddressMapper;
+import com.labmedical.backend.repositories.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
-public interface AddressService {
+public class AddressService {
 
-    ResponseAddressDTO saveAddress(RequestAddressDTO address);
+    @Autowired
+    private AddressRepository addressRepository;
 
-    ResponseAddressDTO findAddresById(Long addressId);
+    @Autowired
+    private AddressMapper addressMapper;
+
+    public ResponseAddressDTO saveAddress(RequestAddressDTO address) {
+        return addressMapper.mapAddressToResponseDTO(this.addressRepository.save(addressMapper.map(address)));
+    }
+
+    public ResponseAddressDTO findAddresById(Long addressId) {
+        return this.addressMapper.mapAddressToResponseDTO(this.addressRepository.findById(addressId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Address not found"))
+        );
+    }
 }
